3839e89cd1dd4f0eb8e89e1e3eef13c30207b5ff
utsapkota <48057340+utsapkota@users.noreply.github.com>
2020-10-14 21:40:13 +0545

    Initial commit

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dataGridView1);
+            this.panel2.Location = new System.Drawing.Point(4, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(351, 227);
+            this.panel2.TabIndex = 5;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.Disable;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGridView1.ShowEditingIcon = false;
+            this.dataGridView1.Size = new System.Drawing.Size(345, 228);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 247);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter Height in M";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 295);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Weight in KG";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(280, 286);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(65, 28);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(355, 324);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(198, 240);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ShortcutsEnabled = false;
+            this.textBox1.Size = new System.Drawing.Size(65, 20);
+            this.textBox1.TabIndex = 20;
+            this.textBox1.TabStop = false;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(117, 240);
+            this.textBox3.MaxLength = 4;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(67, 20);
+            this.textBox3.TabIndex = 1;
+            this.textBox3.Text = "1.64";
+            this.toolTip1.SetToolTip(this.textBox3, "Single Digit Number And Two Decimal Places");
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(117, 291);
+            this.textBox2.MaxLength = 4;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(67, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "59.5";
+            this.toolTip2.SetToolTip(this.textBox2, "Double Digit Numbers And One Decimal Place");
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.label3.Location = new System.Drawing.Point(200, 265);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 18);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "BM Index";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(198, 286);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(65, 28);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(284, 260);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(62, 17);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.Text = "Woman";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(284, 235);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(46, 17);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Man";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.Popup += new System.Windows.Forms.PopupEventHandler(this.toolTip1_Popup);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(357, 329);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "BMI Calculator @utsapkota";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ToolTip toolTip2;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+
+        float height;
+        float weight;
+        float bmi;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            {
+                DataTable dt = new DataTable();
+
+                dt.Columns.Add("Adult");
+                dt.Columns.Add("Women");
+                dt.Columns.Add("Men");
+
+                dt.Rows.Add(new object[] { "Anorexia", "< 17.5 ", "< 17.5" });
+                dt.Rows.Add(new object[] { "Underweight", "<19.1", "<20.7" });
+                dt.Rows.Add(new object[] { "In Normal Range", "19.1-25.8", "20.7-26.4" });
+                dt.Rows.Add(new object[] { "Marginally Overweight", "25.8-27.3", "26.4-27.8" }); ;
+                dt.Rows.Add(new object[] { "Overweight", "27.3-32.3", "27.8-31.1" });
+                dt.Rows.Add(new object[] { "Very Overweight Or Obese", ">32.3", ">31.1" });
+                dt.Rows.Add(new object[] { "Severely Obese", "35 – 40", "35 – 40" });
+                dt.Rows.Add(new object[] { "Morbidly Obese", "40 – 50", "40 – 50" });
+                dt.Rows.Add(new object[] { "Super Obese", " 50 – 60", " 50 – 60" });
+
+                dataGridView1.DataSource = dt;
+            }
+        }
+
+        private void man_bmi()
+        {
+            if (bmi < 17.5)
+                MessageBox.Show("You have anorexia.", "Oh No! Visit a Doctor",
+        MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            if (bmi >= 17.5 && bmi <= 20.7)
+                MessageBox.Show("You are underweight.", "Not Good! Eat More",
+        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            else if (bmi >= 20.7 && bmi <= 26.4)
+                MessageBox.Show("You are in normal range.", "Excellent!",
+        MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else if (bmi >= 26.4 && bmi <= 27.8)
+                MessageBox.Show("You are marginally overweight.", "Excercise More!",
+        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            else if (bmi >= 27.8 && bmi <= 31.1)
+                MessageBox.Show("You are overweight.", "Do Regular Workouts!",
+        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            else if (bmi >= 32.1 && bmi <= 35)
+                MessageBox.Show("You are very overweight/obese.", "Do High Intensity Workouts!",
+        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            else if (bmi >= 35 && bmi <= 40)
+                MessageBox.Show("You are severely obese.", "Oh no! Visit a Doctor",
+        MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            else if (bmi >= 40 && bmi <= 50)
+                MessageBox.Show("You are morbidly obese.", "Oh no! Visit a Doctor",
+        MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            else if (bmi >= 50 && bmi <= 130)
+                MessageBox.Show("You are super obese.", "Oh no! Visit a Doctor",
+       MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            else if (bmi > 130)
+                MessageBox.Show("Human Being With Such BMI Shouldn't Exist", "Something's Wrong!",
+       MessageBoxButtons.OK, MessageBoxIcon.Stop);
+        }
+
+        private void woman_bmi()
+        {
+            if (bmi < 17.5)
+                MessageBox.Show("You have anorexia.", "Oh No! Visit a Doctor",
+        MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            if (bmi >= 17.5 && bmi <= 19.1)
+                MessageBox.Show("You are underweight.", "Not Good! Eat More",
+        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            else if (bmi >= 19.1 && bmi <= 25.8)
+                MessageBox.Show("You are in normal range.", "Excellent!",
+        MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else if (bmi >= 25.8 && bmi <= 27.3)
+                MessageBox.Show("You are marginally overweight.", "Excercise More!",
+        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            else if (bmi >= 27.3 && bmi <= 32.3)
+                MessageBox.Show("You are overweight.", "Do Regular Workouts!",
+        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            else if (bmi >= 32.3 && bmi <= 35)
+                MessageBox.Show("You are very overweight/obese.", "Do High Intensity Workouts!",
+        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            else if (bmi >= 35 && bmi <= 40)
+                MessageBox.Show("You are severely obese.", "Oh no! Visit a Doctor",
+        MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            else if (bmi >= 40 && bmi <= 50)
+                MessageBox.Show("You are morbidly obese.", "Oh no! Visit a Doctor",
+        MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            else if (bmi >= 50 && bmi <= 130)
+                MessageBox.Show("You are super obese.", "Oh no! Visit a Doctor",
+       MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            else if (bmi > 130)
+                MessageBox.Show("Human Being With Such BMI Shouldn't Exist", "Something's Wrong!",
+       MessageBoxButtons.OK, MessageBoxIcon.Stop);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                weight = Single.Parse(textBox2.Text);
+                height = float.Parse(textBox3.Text);
+                height = (float)Math.Round(height, 2);
+                bmi = weight / (height * height);
+                bmi = (float)Math.Round(bmi, 1);
+                // textBox1.Text = String.Format("{0:f1}", bmi);
+                String stringbmi = bmi.ToString();
+                textBox1.Text = stringbmi;
+
+                if (radioButton2.Checked == true)
+                {
+                    woman_bmi();
+                }
+
+
+                if (radioButton1.Checked == true)
+                {
+                    man_bmi();
+                }
+
+            }
+            catch (Exception)
+            {
+                if (textBox2.Text == "")
+                    MessageBox.Show("Please Enter Your Weight");
+
+                if (textBox3.Text == "")
+                    MessageBox.Show("Please Enter Your Height");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            textBox3.Clear();
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            textBox1.Clear();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+
+            if (ch == 46 && textBox3.Text.IndexOf('.') != -1)
+            {
+                e.Handled = true;
+                return;
+            }
+
+            if (!Char.IsDigit(ch) && ch != 8 && ch != 46)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+
+            if (ch == 46 && textBox2.Text.IndexOf('.') != -1)
+            {
+                e.Handled = true;
+                return;
+            }
+
+            if (!Char.IsDigit(ch) && ch != 8 && ch != 46)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void toolTip1_Popup(object sender, PopupEventArgs e)
+        {
+
+        }
+    }
+}
